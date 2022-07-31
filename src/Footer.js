1196376ac35d0a9aad748fae20472db68e730d8a
Nanda <nandakumar.v1602@gmail.com>
2022-08-01 00:56:25 +0530

    project updated....

--- src/Footer.js
+++ src/Footer.js
@@ -6,6 +6,7 @@ import {
   MDBRow,
   MDBRipple,
 } from 'mdb-react-ui-kit';
+import './About.css'
 export default function Footer() {
 
   return (
@@ -20,7 +21,7 @@ export default function Footer() {
               >
                 
                 <a href='https://mediniz.com/list/hospitals/india' style={{textDecoration:'none'}}>
-                <img src='https://media.istockphoto.com/photos/modern-hospital-building-picture-id1312706413?b=1&k=20&m=1312706413&s=170667a&w=0&h=VRi3w2E1UqvCCcK-nDV6mH7FhDZoTU9MM2QKSom96X4=' className='w-100' style={{height:'8rem'}} />
+                <img class="imagine" src='https://media.istockphoto.com/photos/modern-hospital-building-picture-id1312706413?b=1&k=20&m=1312706413&s=170667a&w=0&h=VRi3w2E1UqvCCcK-nDV6mH7FhDZoTU9MM2QKSom96X4=' className='w-100' />
                 <h6 style={{color:"black"}}>Find Hospitals</h6>
                   <div
                     className='mask'
@@ -36,7 +37,7 @@ export default function Footer() {
               >
                 
                 <a href='https://mediniz.com/list/ambulance-services/india' style={{textDecoration:'none'}}>
-                <img src='https://image.winudf.com/v2/image1/Y29tLmdhbWF4aWRjLm11bHRpbGV2ZWxmbHlpbmdhbWJ1bGFuY2VoZF9zY3JlZW5fMF8xNTQxMjQ0OTIwXzA5MA/screen-0.jpg?fakeurl=1&type=.webp' className='w-100' style={{height:'8rem'}}/>
+                <img class="imagine" src='https://image.winudf.com/v2/image1/Y29tLmdhbWF4aWRjLm11bHRpbGV2ZWxmbHlpbmdhbWJ1bGFuY2VoZF9zY3JlZW5fMF8xNTQxMjQ0OTIwXzA5MA/screen-0.jpg?fakeurl=1&type=.webp' className='w-100 ' />
                 <h6 style={{color:"black"}}>Ambulance Services Hospitals</h6>
                   <div
                     className='mask'
@@ -51,7 +52,7 @@ export default function Footer() {
                 className='bg-image hover-overlay shadow-1-strong rounded'
               >
                 <a href='https://mediniz.com/list/blood-banks/india' style={{textDecoration:'none'}}>
-                <img src='https://cdn.pixabay.com/photo/2020/07/21/19/41/blood-5427229__340.jpg' className='w-100' style={{height:'8rem'}}/>
+                <img class="imagine" src='https://cdn.pixabay.com/photo/2020/07/21/19/41/blood-5427229__340.jpg' className='w-100 ' />
                 <h6 style={{color:"black"}}>Blood Banks</h6>
                   <div
                     className='mask'
@@ -66,7 +67,7 @@ export default function Footer() {
                 className='bg-image hover-overlay shadow-1-strong rounded'
               >
                 <a href='https://mediniz.com/list/health-ngo-foundation/india' style={{textDecoration:'none'}}>
-                <img src='https://www.giveindia.org/blog/wp-content/uploads/2021/12/10-NGOs-which-have-revolutionised-Healthcare-in-India-Banner.jpg' className='w-100' style={{height:'8rem'}} />
+                <img  class="imagine"src='https://www.giveindia.org/blog/wp-content/uploads/2021/12/10-NGOs-which-have-revolutionised-Healthcare-in-India-Banner.jpg' className='w-100'  />
                 <h6 style={{color:"black"}}>Health Groups And NGOs In India</h6>
                   <div
                     className='mask'
@@ -81,7 +82,7 @@ export default function Footer() {
                 className='bg-image hover-overlay shadow-1-strong rounded'
               >
                 <a href='https://mediniz.com/list/diagnostic-centers/india' style={{textDecoration:'none'}}>
-                <img src='https://documents.labsystemsdx.com/category/large/148051424743-Infectious-Disease-Tests.jpg' className='w-100' style={{height:'8rem'}} />
+                <img class="imagine" src='https://documents.labsystemsdx.com/category/large/148051424743-Infectious-Disease-Tests.jpg' className='w-100'  />
                 <h6 style={{color:"black"}}>Diagnostic Centers</h6>
                   <div
                     className='mask'
@@ -96,7 +97,7 @@ export default function Footer() {
                 className='bg-image hover-overlay shadow-1-strong rounded'
               >
                 <a href='https://mediniz.com/list/cancer/hospitals/india' style={{textDecoration:'none'}}>
-                <img src='https://www.mskcc.org/sites/default/files/styles/large/public/node/19243/3x2/biopsy_surgery_1200x800.jpg' className='w-100' style={{height:'8rem'}} />
+                <img class="imagine" src='https://www.mskcc.org/sites/default/files/styles/large/public/node/19243/3x2/biopsy_surgery_1200x800.jpg' className='w-100'  />
                 <h6 style={{color:"black"}}>Cancer Help Centers</h6>
                   <div
                     className='mask'
